f1ae3331e54e0591ff3fe61a0866ddcd75acff92
Max Fatouros <git.q13kx@slmail.me>
2025-09-08 18:01:56 +0200

    initial working global minimization

--- psi_praktikum/__main__.py
+++ psi_praktikum/__main__.py
@@ -25,20 +25,47 @@ plt.style.use(PAPER)
 # plt.rcParams['text.usetex'] = True
 
 
-def double_exponential(t, N_0, t_mu, t_pi):
+def ff3(t, N_0, t_mu, t_pi):
     t_shifted = t
     return N_0 * (
         np.exp(- (t_shifted / t_mu))
         - np.exp(- (t_shifted / t_pi))
     )
 
-# def double_exponential(t, N_0, t0, t_mu, t_pi):
-#     t_shifted = t - t0
-#     return N_0 * (
-#         np.exp(- (t_shifted / t_mu))
-#         - np.exp(- (t_shifted / t_pi))
-#     )
+def ff4a(t, t0, N_0, t_mu, t_pi):
+    t_shifted = t - t0
+    f1 = N_0 * (
+        np.exp(- (t_shifted / t_mu))
+        - np.exp(- (t_shifted / t_pi))
+    )
+    return f1
 
+def ff4b(t, p0, N_0, t_mu, t_pi):
+    t_shifted = t
+    f1 = N_0 * (
+        np.exp(- (t_shifted / t_mu))
+        - np.exp(- (t_shifted / t_pi))
+    )
+    f2 = f1 + p0
+    return f1
+
+
+def ff(ts: np.ndarray, p0, p1, p2, p3, N0, t0, t_mu, t_pi):
+    """
+    The whole time-range must be passed,
+    otherwise the convolutions probably won't work.
+    """
+    ts = ts - t0
+    noise_gaussian = p0 * np.exp(-(ts**2) / (2 * p1**2))
+    double_exponential = N0 * (
+        np.exp(- (ts / t_mu))
+        - np.exp(- (ts / t_pi))
+    )
+    f1 = np.convolve(double_exponential, noise_gaussian, mode='same')
+    f2 = f1 + p2  # offset for random coincidences
+    hadronic_gaussian = p3 * np.exp(-(ts**2) / (2 * p1**2))
+    f3 = f2 + hadronic_gaussian
+    return f3
 
 def sample_double_exponential(N_0, t_mu, t_pi):
     """
@@ -66,8 +93,18 @@ def parse_data(filename: str) -> np.ndarray:
     return np.asarray(output)
 
 
-def fit_simulated(N_0):
+def fit_simulated(
+    N_0=100_000,
+    fit_function=ff,
+    bounds=(
+        (0,0,-np.inf,0, 0,0,0,0),
+        (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)
+    ),
+):
     samples = sample_double_exponential(N_0, constants.T_MU, constants.T_PI)
+    # samples += 1.75e-6
+    # samples += 25e-6
+    # samples += np.random.normal(0, 0.1, N_0)
 
     # samples = samples[samples < 3e-6]
     # samples = samples[1e-6 < samples]
@@ -78,32 +115,63 @@ def fit_simulated(N_0):
 
     # TODO: give uncertainty in the y-values
     #       (square root of the number of entries)
-    popt, pcov = scipy.optimize.curve_fit(
-        double_exponential,
-        xs,
-        hist,
-        # p0=(0,0,0),
+    # NOTE: the minimization method changes if you pass initial parameters (p0).
+    #       I've had better luck by not setting it.
+
+    # sigma = np.sqrt(
+    #     [
+    #         e if e != 0 else 1
+    #         for e in hist
+    #     ]
+    # )
+
+
+    # hist[hist==0] = np.nan
+    fhist = hist.astype(float)
+    # fhist[hist==0] = np.nan
+    # sigma = np.sqrt(fhist)
+
+    def f(popts):
+        residuals = fit_function(xs, *popts) - fhist
+        return np.sum(residuals**2)
+
+    # popt, pcov = scipy.optimize.curve_fit(
+    #     fit_function,
+    #     xs,
+    #     fhist,
+    #     bounds=bounds,
+    #     sigma=sigma,
+    #     nan_policy='omit',
+    # )
+
+
+    fit = scipy.optimize.dual_annealing(
+        f,
         bounds=(
-            (0, 0, 0),
-            (np.inf, np.inf, np.inf)
+            (0., 1e10),
+            (0., 1e-2),
+            (0., 1e-5),
         )
     )
 
-    print(f'{popt=}')
-    plt.bar(xs, hist, 0.8 * (bin_edges[1:] - bin_edges[:-1]))
+    print(fit.x)
 
-    # ts = np.linspace(0, 1e-5, num=10_000)
-    # true_data = 500 * double_exponential(ts, constants.T_MU, constants.T_PI)
-    # hist, bin_edges = np.histogram(true_data, )
-    # plt.scatter(ts, true_data)
-
-    # plt.plot(xs, double_exponential(xs, *popt))
-    plt.show()
+    # print(f'{popt=}')
+    # plt.bar(xs, hist, 0.8 * (bin_edges[1:] - bin_edges[:-1]))
+    # plt.plot(xs, fit_function(xs, *popt))
+    # plt.show()
 
 
-def fit_data(filename):
+def fit_data(
+    filename,
+    fit_function=ff,
+    bounds=(
+        (0,0,-np.inf,0, 0,0,0,0),
+        (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)
+    ),
+):
     # TODO: cut the right hand overflow off
-    data = parse_data(filename)[100:-300]
+    data = parse_data(filename)[100:-200]
 
     bin_edges = range(len(data) + 1)
     bin_edge_times = apply_fit(bin_edges, constants.P_1, constants.P_2)[:, 0]
@@ -117,16 +185,30 @@ def fit_data(filename):
     plt.xlabel('Decay time [microseconds]')
 
     popt, pcov = scipy.optimize.curve_fit(
-        double_exponential,
+        ff,
         times,
         data,
-        bounds=(
-            (0, 0, 0, 0),
-            (np.inf, np.inf, np.inf, np.inf),
-        ),
+        bounds=bounds,
+        # bounds=(
+        #     (0, 0, 0, 0, 0, -np.inf, 0, 0),
+        #     (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)
+        #     # (0,)*8,
+        #     # (np.inf,)*8,
+        # ),
+        # bounds=(
+        #     (0, 0, 0, 0, 0),
+        #     (np.inf, np.inf, np.inf, np.inf, np.inf),
+            # (0,)*8,
+            # (np.inf,)*8,
+        # ),
     )
 
     print(popt)
+
+
+    plt.plot(times, fit_function(times, *popt))
+
+
     plt.show()
 
 
@@ -176,7 +258,7 @@ def fit_calibration(
     Returns:
         The parameters of the fit, ordered from highest to lowest polynomial order.
     """
-    data = parse_data(ilename)
+    data = parse_data(filename)
     data = smooth_peaks(data)
     bin_numbers = np.arange(0, len(data))
     peak_bin_indices = find_peaks(data)
@@ -198,6 +280,7 @@ def fit_calibration(
         # )
         plt.show()
 
+    # TODO: find error
     parameters = linear_fit(peak_bin_indices, times)
 
     if visualize:
@@ -211,16 +294,32 @@ def fit_calibration(
         plt.xlabel('bin number')
         plt.ylabel('time [microseconds]')
         plt.show()
+
     return parameters
 
 def main():
 
     print(">>> sim")
-    fit_simulated(1_000_000)
+    fit_simulated(
+        fit_function=ff3,
+        # bounds=(
+        #     (0,0,0),
+        #     (np.inf, np.inf, np.inf)
+        # )
+    )
+    # fit_simulated(
+    #     fit_function=ff4a,
+    #     bounds=(
+    #         (0,0,0,0),
+    #         (5e-6, np.inf, np.inf, np.inf)
+    #     )
+    # )
 
     # print(">>> data")
     # fit_data(
     #     "stop_S6andS7_delay_1_5_mus_fs12_50and100mm_30min.Spe",
+    #     # "PSI_lab_2025/stop_S6andS7_delay_1_5_mus_fs12_135mm_60min_timinggivenbys7.Spe"
+    #     # "PSI_lab_2025/stop_S6andS7_delay_1_5_mus_fs12_135mm_timinggivenbys7_CFD_allstat.Spe",
     # )
 
     # fit_calibration(
